5bd9ed457e68379d212c4183734fa306c5a98524
Davin-01 <moengadavid90@gmail.com>
2026-07-27 22:56:56 +0300

    the complete functional habit tracker site

---
+++ task-manager/src/TaskInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TaskInput(props) {
+    return (
+        <div>
+            <input type="text" placeholder="Enter a new Task"/>
+            <button>Add Task</button>
+        </div>
+    )
+};
+
+export default TaskInput;
